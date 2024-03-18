d9962480d4a3aa9d2bb0da6128b05296a8d898da
Mateusz Zając <matzayonc@gmail.com>
2024-03-18 21:03:01 +0100

    websocket testing page

--- main/background.ts
+++ main/background.ts
@@ -1,23 +1,24 @@
-import path from 'path'
-import { app, ipcMain } from 'electron'
-import serve from 'electron-serve'
-import { createWindow } from './helpers'
+import path from "path"
+import { app, ipcMain } from "electron"
+import serve from "electron-serve"
+import { createWindow } from "./helpers"
 
-const os = require('os');
-const cors = require('cors');
+const os = require("os")
+const cors = require("cors")
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express")
+const bodyParser = require("body-parser")
 
-const express_app = express();
-const PORT = 3000;
+const express_app = express()
+const PORT = 3000
 
+express_app.use(bodyParser.urlencoded({ extended: true }))
 
-express_app.use(bodyParser.urlencoded({ extended: true }));
-express_app.use(cors());
+express_app.use(bodyParser.urlencoded({ extended: true }))
+express_app.use(cors())
 
-express_app.get('/', (req, res) => {
-  res.send(`
+express_app.get("/", (req, res) => {
+	res.send(`
       <html>
           <head>
               <title>Podaj nick</title>
@@ -30,37 +31,39 @@ express_app.get('/', (req, res) => {
               </form>
           </body>
       </html>
-  `);
-});
+  `)
+})
 
 function getIpAddress() {
-  const networkInterfaces = os.networkInterfaces();
-  let ipv4Address;
-  Object.keys(networkInterfaces).forEach(interfaceName => {
-    networkInterfaces[interfaceName].forEach(networkInterface => {
-      if (networkInterface.family === 'IPv4' && !networkInterface.internal) {
-        ipv4Address = networkInterface.address;
-      }
-    });
-  });
-  return ipv4Address;
+	const networkInterfaces = os.networkInterfaces()
+	let ipv4Address
+	Object.keys(networkInterfaces).forEach(interfaceName => {
+		networkInterfaces[interfaceName].forEach(networkInterface => {
+			if (
+				networkInterface.family === "IPv4" &&
+				!networkInterface.internal
+			) {
+				ipv4Address = networkInterface.address
+			}
+		})
+	})
+	return ipv4Address
 }
 
-express_app.get('/ipaddress', (req, res) => {
-  const networkInterfaces = os.networkInterfaces();
-  let ipv4Address = getIpAddress();
-  if (ipv4Address) {
-    res.json(ipv4Address );
-  } else {
-    res.status(500).json({ error: 'Nie udało się znaleźć adresu IPv4' });
-  }
-});
+express_app.get("/ipaddress", (req, res) => {
+	const networkInterfaces = os.networkInterfaces()
+	let ipv4Address = getIpAddress()
+	if (ipv4Address) {
+		res.json(ipv4Address)
+	} else {
+		res.status(500).json({ error: "Nie udało się znaleźć adresu IPv4" })
+	}
+})
 
-
-express_app.post('/submit', (req, res) => {
-    var nick = req.body.nick;
-    console.log(nick);
-    res.send(`
+express_app.post("/submit", (req, res) => {
+	var nick = req.body.nick
+	console.log(nick)
+	res.send(`
         <html>
             <head>
                 <title>Witaj!</title>
@@ -70,48 +73,51 @@ express_app.post('/submit', (req, res) => {
                 <p>Dziękujemy za podanie nicku.</p>
             </body>
         </html>
-    `);
-});
+    `)
+})
 
+express_app.get("/websockets", (req, res) => {
+	res.sendFile("static/websockets.html", { root: __dirname })
+})
 
 express_app.listen(PORT, () => {
-    console.log(`Serwer działa na porcie ${PORT}`);
-});
+	console.log(`Serwer działa na porcie ${PORT}`)
+})
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === "production"
 
 if (isProd) {
-  serve({ directory: 'app' })
+	serve({ directory: "app" })
 } else {
-  app.setPath('userData', `${app.getPath('userData')} (development)`)
+	app.setPath("userData", `${app.getPath("userData")} (development)`)
 }
 
 ;(async () => {
-  await app.whenReady()
+	await app.whenReady()
 
-  const mainWindow = createWindow('main', {
-    width: 1000,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      sandbox: false,
-      nodeIntegration: true
-    },
-  })
+	const mainWindow = createWindow("main", {
+		width: 1000,
+		height: 600,
+		webPreferences: {
+			preload: path.join(__dirname, "preload.js"),
+			sandbox: false,
+			nodeIntegration: true,
+		},
+	})
 
-  if (isProd) {
-    await mainWindow.loadURL('app://./home')
-  } else {
-    const port = process.argv[2]
-    await mainWindow.loadURL(`http://localhost:${port}/home`)
-    mainWindow.webContents.openDevTools()
-  }
+	if (isProd) {
+		await mainWindow.loadURL("app://./home")
+	} else {
+		const port = process.argv[2]
+		await mainWindow.loadURL(`http://localhost:${port}/home`)
+		mainWindow.webContents.openDevTools()
+	}
 })()
 
-app.on('window-all-closed', () => {
-  app.quit()
+app.on("window-all-closed", () => {
+	app.quit()
 })
 
-ipcMain.on('message', async (event, arg) => {
-  event.reply('message', `${arg} World!`)
+ipcMain.on("message", async (event, arg) => {
+	event.reply("message", `${arg} World!`)
 })
