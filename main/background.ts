57ae0145ffda6b2a7e4da19ddb094dba6e991f14
Mateusz Zając <matzayonc@gmail.com>
2024-03-18 21:03:01 +0100

    websocket testing page

--- main/background.ts
+++ main/background.ts
@@ -1,19 +1,18 @@
-import path from 'path'
-import { app, ipcMain } from 'electron'
-import serve from 'electron-serve'
-import { createWindow } from './helpers'
+import path from "path"
+import { app, ipcMain } from "electron"
+import serve from "electron-serve"
+import { createWindow } from "./helpers"
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express")
+const bodyParser = require("body-parser")
 
-const express_app = express();
-const PORT = 3000;
+const express_app = express()
+const PORT = 3000
 
+express_app.use(bodyParser.urlencoded({ extended: true }))
 
-express_app.use(bodyParser.urlencoded({ extended: true }));
-
-express_app.get('/', (req, res) => {
-  res.send(`
+express_app.get("/", (req, res) => {
+	res.send(`
       <html>
           <head>
               <title>Podaj nick</title>
@@ -26,14 +25,13 @@ express_app.get('/', (req, res) => {
               </form>
           </body>
       </html>
-  `);
-});
+  `)
+})
 
-
-express_app.post('/submit', (req, res) => {
-    var nick = req.body.nick;
-    console.log(nick);
-    res.send(`
+express_app.post("/submit", (req, res) => {
+	var nick = req.body.nick
+	console.log(nick)
+	res.send(`
         <html>
             <head>
                 <title>Witaj!</title>
@@ -43,46 +41,49 @@ express_app.post('/submit', (req, res) => {
                 <p>Dziękujemy za podanie nicku.</p>
             </body>
         </html>
-    `);
-});
+    `)
+})
 
+express_app.get("/websockets", (req, res) => {
+	res.sendFile("static/websockets.html", { root: __dirname })
+})
 
 express_app.listen(PORT, () => {
-    console.log(`Serwer działa na porcie ${PORT}`);
-});
+	console.log(`Serwer działa na porcie ${PORT}`)
+})
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === "production"
 
 if (isProd) {
-  serve({ directory: 'app' })
+	serve({ directory: "app" })
 } else {
-  app.setPath('userData', `${app.getPath('userData')} (development)`)
+	app.setPath("userData", `${app.getPath("userData")} (development)`)
 }
 
 ;(async () => {
-  await app.whenReady()
+	await app.whenReady()
 
-  const mainWindow = createWindow('main', {
-    width: 1000,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  })
+	const mainWindow = createWindow("main", {
+		width: 1000,
+		height: 600,
+		webPreferences: {
+			preload: path.join(__dirname, "preload.js"),
+		},
+	})
 
-  if (isProd) {
-    await mainWindow.loadURL('app://./home')
-  } else {
-    const port = process.argv[2]
-    await mainWindow.loadURL(`http://localhost:${port}/home`)
-    mainWindow.webContents.openDevTools()
-  }
+	if (isProd) {
+		await mainWindow.loadURL("app://./home")
+	} else {
+		const port = process.argv[2]
+		await mainWindow.loadURL(`http://localhost:${port}/home`)
+		mainWindow.webContents.openDevTools()
+	}
 })()
 
-app.on('window-all-closed', () => {
-  app.quit()
+app.on("window-all-closed", () => {
+	app.quit()
 })
 
-ipcMain.on('message', async (event, arg) => {
-  event.reply('message', `${arg} World!`)
+ipcMain.on("message", async (event, arg) => {
+	event.reply("message", `${arg} World!`)
 })
