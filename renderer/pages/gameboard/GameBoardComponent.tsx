2df915283d5b2edf34d30b817990f99f0a865cba
irosikoni <irosikoni@gmail.com>
2024-03-21 00:18:20 +0100

    board done

--- renderer/pages/gameboard/GameBoardComponent.tsx
+++ renderer/pages/gameboard/GameBoardComponent.tsx
@@ -1,44 +1,204 @@
-import { GameBoardConfiguration } from ".";
+import { twMerge } from "tailwind-merge";
+import { GameBoardConfiguration, Player } from ".";
+
+type BoardFieldSpecialty =
+  | "question"
+  | "good"
+  | "bad"
+  | "finish"
+  | "start"
+  | "empty";
+
+type BoardField = {
+  index: number;
+  colClass: number;
+  rowClass: number;
+  type: BoardFieldSpecialty;
+};
+
+function mapTypeOnColor(type: BoardFieldSpecialty) {
+  switch (type) {
+    case "question":
+      return "bg-secondary";
+    case "good":
+      return "bg-childGreen";
+    case "bad":
+      return "bg-red-500";
+    case "empty":
+      return "bg-childWhite";
+  }
+}
+
+function changePosition(
+  players: Player[],
+  currentPlayer: Player,
+  newPosition: number
+) {
+  const newPlayers = players.map((player) => {
+    if (player.orderId === currentPlayer.orderId) {
+      return { ...player, position: newPosition };
+    }
+    return player;
+  });
+  return newPlayers;
+}
 
 export function GameBoardComponent({
-  numberOfQuestionFields = 15,
-  numberOfGoodSpecialFields = 3,
-  numberOfBadSpecialFields = 3,
-}: GameBoardConfiguration) {
-  const numberOfColumns = Math.floor(numberOfQuestionFields / 3) - 1;
+  configuration: {
+    numberOfQuestionFields = 15,
+    numberOfGoodSpecialFields = 3,
+    numberOfBadSpecialFields = 3,
+  },
+  players,
+}: {
+  configuration: GameBoardConfiguration;
+  players: Player[];
+}) {
+  const numberOfColumns = Math.floor((numberOfQuestionFields * 2) / 3) - 1;
+  const totalFields = 2 * numberOfQuestionFields + 2;
+
+  const getBoardFieldSpecialty = (index: number): BoardFieldSpecialty => {
+    if (
+      index % (numberOfBadSpecialFields + numberOfGoodSpecialFields - 1) ===
+      0
+    )
+      return "good";
+    return index % 2 === 0 ? "question" : "empty"; // Special or question
+  };
+
+  let currentColumn = 1;
+  let currentRow = 2;
+  let target = numberOfColumns - 2;
+  let upOrDown = "up";
+  let colClass = currentColumn;
+  let rowClass = currentRow;
+
+  const gridPositions: {
+    index: number;
+    colClass: number;
+    rowClass: number;
+    type: BoardFieldSpecialty;
+  }[] = [];
+  gridPositions.push({ index: 1, colClass: 1, rowClass: 1, type: "start" });
+
+  const getGridPosition = (index: number, numberOfColumns: number) => {
+    colClass = currentColumn;
+    rowClass = currentRow;
+    if (index === target) {
+      currentColumn++;
+      colClass = currentColumn;
+      currentColumn++;
+      target += numberOfColumns - 3;
+      if (upOrDown === "up") upOrDown = "down";
+      else upOrDown = "up";
+      gridPositions.push({
+        index,
+        colClass,
+        rowClass,
+        type: getBoardFieldSpecialty(index),
+      });
+      return { colClass, rowClass, type: getBoardFieldSpecialty(index) };
+    }
+    if (upOrDown === "down") {
+      rowClass = currentRow;
+      if (index === target - 1) {
+        gridPositions.push({
+          index,
+          colClass,
+          rowClass,
+          type: getBoardFieldSpecialty(index),
+        });
+        return { colClass, rowClass, type: getBoardFieldSpecialty(index) };
+      }
+      currentRow--;
+      gridPositions.push({
+        index,
+        colClass,
+        rowClass,
+        type: getBoardFieldSpecialty(index),
+      });
+      return { colClass, rowClass, type: getBoardFieldSpecialty(index) };
+    }
+    rowClass = currentRow;
+    if (index === target - 1) {
+      gridPositions.push({
+        index,
+        colClass,
+        rowClass,
+        type: getBoardFieldSpecialty(index),
+      });
+      return { colClass, rowClass, type: getBoardFieldSpecialty(index) };
+    }
+    currentRow++;
+    gridPositions.push({
+      index,
+      colClass,
+      rowClass,
+      type: getBoardFieldSpecialty(index),
+    });
+    return { colClass, rowClass, type: getBoardFieldSpecialty(index) };
+  };
+
+  console.log(gridPositions);
+
   return (
-    <div className="w-full h-full grid grid-cols-9 grid-rows-7 gap-4">
-      <div className="bg-childBlack">1</div>
-      <div className="col-start-1 row-start-2 bg-childWhite">2</div>
-      <div className="col-start-1 row-start-3 bg-secondary">3</div>
-      <div className="col-start-1 row-start-4 bg-childWhite">4</div>
-      <div className="col-start-1 row-start-5 bg-secondary">5</div>
-      <div className="col-start-1 row-start-6 bg-childWhite">6</div>
-      <div className="col-start-2 row-start-6 bg-secondary">7</div>
-      <div className="col-start-3 row-start-6 bg-childWhite">8</div>
-      <div className="col-start-3 row-start-5 bg-secondary">9</div>
-      <div className="col-start-3 row-start-4 bg-childWhite">10</div>
-      <div className="col-start-3 row-start-3 bg-secondary">11</div>
-      <div className="col-start-3 row-start-2 bg-childWhite">12</div>
-      <div className="col-start-4 row-start-2 bg-secondary">13</div>
-      <div className="col-start-5 row-start-2 bg-childWhite">14</div>
-      <div className="col-start-5 row-start-3 bg-secondary">15</div>
-      <div className="col-start-5 row-start-4 bg-childWhite">16</div>
-      <div className="col-start-5 row-start-5 bg-secondary">17</div>
-      <div className="col-start-5 row-start-6 bg-childWhite">18</div>
-      <div className="col-start-6 row-start-6 bg-secondary">19</div>
-      <div className="col-start-7 row-start-6 bg-childWhite">20</div>
-      <div className="col-start-7 row-start-5 bg-secondary">21</div>
-      <div className="col-start-7 row-start-4 bg-childWhite">22</div>
-      <div className="col-start-7 row-start-3 bg-secondary">23</div>
-      <div className="col-start-7 row-start-2 bg-childWhite">24</div>
-      <div className="col-start-8 row-start-2 bg-secondary">25</div>
-      <div className="col-start-9 row-start-2 bg-childWhite">26</div>
-      <div className="col-start-9 row-start-3 bg-secondary">27</div>
-      <div className="col-start-9 row-start-4 bg-childWhite">28</div>
-      <div className="col-start-9 row-start-5 bg-secondary">29</div>
-      <div className="col-start-9 row-start-6 bg-childWhite">30</div>
-      <div className="col-start-9 row-start-7 bg-childBlack">31</div>
+    <div className={`w-full h-full grid gap-4`}>
+      <div className="bg-childBlack row-start-1">1</div>
+      {Array.from({ length: totalFields - 3 }, (_, index) => {
+        const {
+          colClass: col,
+          rowClass: row,
+          type,
+        } = getGridPosition(index + 2, numberOfColumns);
+        return (
+          <div
+            key={index}
+            style={{
+              gridColumnStart: col,
+              gridRowStart: row,
+            }}
+            className={twMerge(
+              mapTypeOnColor(type),
+              "h-32 w-32 flex flex-wrap justify-center content-center items-center p-3"
+            )}
+          >
+            {players[0].position === index && (
+              <div className="m-0.5 bg-gray-800 w-8 h-8 rounded-full" />
+            )}
+
+            {players[1].position === index && (
+              <div className="m-0.5 bg-gray-600 w-8 h-8 rounded-full" />
+            )}
+            {players[2].position === index && (
+              <div className="m-0.5 bg-gray-400 w-8 h-8 rounded-full" />
+            )}
+            {players[3].position === index && (
+              <div className="m-0.5 bg-blue-400 w-8 h-8 rounded-full" />
+            )}
+            {players[4].position === index && (
+              <div className="m-0.5 bg-blue-600 w-8 h-8 rounded-full" />
+            )}
+            {players[5].position === index && (
+              <div className="m-0.5 bg-blue-800 w-8 h-8 rounded-full" />
+            )}
+          </div>
+        );
+      })}
+      {gridPositions.push({
+        index: totalFields + 1,
+        colClass: 9,
+        rowClass: 7,
+        type: "finish",
+      })}
+      <button
+        className={`bg-childBlack col-start-9 row-start-7`}
+        onClick={(event) => {
+          event.preventDefault();
+          changePosition(players, players[0], players[0].position + 2);
+        }}
+      >
+        1
+      </button>
     </div>
   );
 }
--- renderer/pages/gameboard/index.tsx
+++ renderer/pages/gameboard/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { GameBoardComponent } from "./GameBoardComponent";
 
-type Player = {
+export type Player = {
   orderId: number;
   nick: string;
   score: number;
@@ -23,18 +23,26 @@ function setNextPlayer(currentPlayer: Player, players: Player[]) {
 }
 
 export default function GameBoard(
-  players: Player[],
+  // players: Player[],
   configuration: GameBoardConfiguration
 ) {
-  if (!players) {
-    return <div>Nie ma z kim grać...</div>;
-  }
+  // if (!players) {
+  //   return <div>Nie ma z kim grać...</div>;
+  // }
 
-  const [currentPlayer, setCurrentPlayer] = useState<Player>(players[0]);
+  // const [currentPlayer, setCurrentPlayer] = useState<Player>(players[0]);
+  const players = [
+    { orderId: 0, nick: "Gracz 1", score: 0, position: 3 },
+    { orderId: 1, nick: "Gracz 2", score: 0, position: 6 },
+    { orderId: 2, nick: "Gracz 3", score: 0, position: 9 },
+    { orderId: 3, nick: "Gracz 4", score: 0, position: 10 },
+    { orderId: 4, nick: "Gracz 5", score: 0, position: 13 },
+    { orderId: 5, nick: "Gracz 6", score: 0, position: 13 },
+  ];
 
   return (
     <div className="w-[100vw] h-[100vh] p-32">
-      <GameBoardComponent {...configuration} />
+      <GameBoardComponent configuration={configuration} players={players} />
     </div>
   );
 }
