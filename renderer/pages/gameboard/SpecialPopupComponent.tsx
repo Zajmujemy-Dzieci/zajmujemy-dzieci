2ce14add6ad8423dd3fb228fd4f59112446313ae
Radosław Niżnik <410317@student.agh.edu.pl>
2024-04-07 09:51:07 +0200

    start making popup

---
+++ renderer/pages/gameboard/SpecialPopupComponent.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+type SpecialPopupProps = {
+  text: string;
+  onClose: () => void;
+};
+
+export default function SpecialPopupComponent({
+  text,
+  onClose,
+}: SpecialPopupProps) {
+  return (
+    
+  );
+}
