430b2cc1498a9e570d808d7df1aa5f7f1022adf8
Mateusz Zając <matzayonc@gmail.com>
2024-04-17 13:45:48 +0200

    formatting

--- main/helpers/messages.ts
+++ main/helpers/messages.ts
@@ -1,237 +1,244 @@
-import game from './state'
-import Client from './client'
+import game from "./state";
+import Client from "./client";
 
 export interface ClientMessage {
-	type: "register" | "ping" | "pong" | "dice" | "answer" | "regPawn" | "movePawn" | "question" | 'NICK' | 'throwDice'
+  type:
+    | "register"
+    | "ping"
+    | "pong"
+    | "dice"
+    | "answer"
+    | "regPawn"
+    | "movePawn"
+    | "question"
+    | "NICK"
+    | "throwDice";
 }
 
 export interface PongMessage extends ClientMessage {
-	type: "pong"
-	nick: string
+  type: "pong";
+  nick: string;
 }
 
 export interface RegisterMessage extends ClientMessage {
-	type: "register"
-	nick: string
+  type: "register";
+  nick: string;
 }
 
 export interface DiceThrowMessage extends ClientMessage {
-	type: "dice"
-	dice: 1 | 2 | 3 | 4 | 5 | 6
-	nick: string
+  type: "dice";
+  dice: 1 | 2 | 3 | 4 | 5 | 6;
+  nick: string;
 }
 
 export interface AnswerMessage extends ClientMessage {
-	type: "answer"
-	answer: string
+  type: "answer";
+  answer: string;
 }
-
 
 export interface ServerMessage {
-	type: "pong" | "throwDice" | "registered" | "timeout"
+  type: "pong" | "throwDice" | "registered" | "timeout";
 }
 
-
 export interface PawnRegisterMessage extends ClientMessage {
-	type: "regPawn"
-	nick: string
+  type: "regPawn";
+  nick: string;
 }
 
 export interface MovePawnMessage extends ClientMessage {
-	type: "movePawn"
-	fieldsToMove: number,
-	nick: string
+  type: "movePawn";
+  fieldsToMove: number;
+  nick: string;
 }
 
 export interface QuestionMessage extends ClientMessage {
-	type: "question",
-	nick: string
+  type: "question";
+  nick: string;
 }
 
 export interface NickMessage extends ClientMessage {
-	type: "NICK",
-	nick: string
+  type: "NICK";
+  nick: string;
 }
 
-
 export interface HostMessage {
-	type: "pong" | "newPlayer"
+  type: "pong" | "newPlayer";
 }
 
 export interface NewPlayerMessage extends HostMessage {
-	type: "newPlayer"
-	nick: string
+  type: "newPlayer";
+  nick: string;
 }
-
 
 export const handleMessage = (msg: ClientMessage, ws: WebSocket) => {
-	switch (msg.type) {
-		case "ping":
-			handlePing(ws)
-			break
+  switch (msg.type) {
+    case "ping":
+      handlePing(ws);
+      break;
 
-		case "register":
-			const registerMsg = msg as RegisterMessage
-			handleRegister(registerMsg, ws)
-			break
+    case "register":
+      const registerMsg = msg as RegisterMessage;
+      handleRegister(registerMsg, ws);
+      break;
 
-		case "dice":
-			const diceMsg = msg as DiceThrowMessage
-			handleDiceThrow(diceMsg)
-			break
+    case "dice":
+      const diceMsg = msg as DiceThrowMessage;
+      handleDiceThrow(diceMsg);
+      break;
 
-		case "answer":
-			const answerMsg = msg as AnswerMessage
-			handleAnswer(answerMsg)
-			break
-		
-		case "regPawn":
-			const pawnMsg = msg as PawnRegisterMessage
-			handlePawnRegister(pawnMsg, ws)
-			break
-		
-		case "movePawn":
-			const movePawnMsg = msg as MovePawnMessage
-			handleMovePawn(movePawnMsg.nick, movePawnMsg.fieldsToMove);
-			break
+    case "answer":
+      const answerMsg = msg as AnswerMessage;
+      handleAnswer(answerMsg);
+      break;
 
-		case "question":
-			const questionMsg = msg as QuestionMessage
-			handleQuestion(questionMsg);
-			break
+    case "regPawn":
+      const pawnMsg = msg as PawnRegisterMessage;
+      handlePawnRegister(pawnMsg, ws);
+      break;
 
-		case "pong":
-			const keepAliveMessage = msg as PongMessage
- 			game.clients.get(keepAliveMessage.nick)?.handlePong()
-			break		
+    case "movePawn":
+      const movePawnMsg = msg as MovePawnMessage;
+      handleMovePawn(movePawnMsg.nick, movePawnMsg.fieldsToMove);
+      break;
 
-		default:
-			console.error("Unknown message type", JSON.stringify(msg))
-	}
-}
+    case "question":
+      const questionMsg = msg as QuestionMessage;
+      handleQuestion(questionMsg);
+      break;
+
+    case "pong":
+      const keepAliveMessage = msg as PongMessage;
+      game.clients.get(keepAliveMessage.nick)?.handlePong();
+      break;
+
+    default:
+      console.error("Unknown message type", JSON.stringify(msg));
+  }
+};
 
 export const throwDice = (nick: string) => {
-	const ws = game.clients.get(nick)
+  const ws = game.clients.get(nick);
 
-	if (!ws) {
-		console.error("No such ws", nick)
-		return
-	}
+  if (!ws) {
+    console.error("No such ws", nick);
+    return;
+  }
 
-	ws.send({ type: "throwDice" })
-}
+  ws.send({ type: "throwDice" });
+};
 
 const handlePing = (ws: WebSocket) => {
-	console.log("Ping")
-	ws.send(JSON.stringify({ type: "pong" }))
-}
+  console.log("Ping");
+  ws.send(JSON.stringify({ type: "pong" }));
+};
 
-const nicks = ["żółw", "wiewiórka", "szynszyla", "pies", "kot", "słoń"]
-let currentTurn = 0
+const nicks = ["żółw", "wiewiórka", "szynszyla", "pies", "kot", "słoń"];
+let currentTurn = 0;
 
 const handleRegister = (msg: RegisterMessage, ws: WebSocket) => {
-	const client = new Client(ws, msg.nick)
-	if (msg.nick === "host") {
-		if (game.clients.has("host")) {
-			console.error("Host already registered")
-			return
-		}
-		game.clients.set("host", client)
-		console.log("Host registered")
-		ws.send(JSON.stringify({ type: "registered" }))
-		return
-	}
+  const client = new Client(ws, msg.nick);
+  if (msg.nick === "host") {
+    if (game.clients.has("host")) {
+      console.error("Host already registered");
+      return;
+    }
+    game.clients.set("host", client);
+    console.log("Host registered");
+    ws.send(JSON.stringify({ type: "registered" }));
+    return;
+  }
 
-	if (game.isInProgress()) {
-		const replacedClient = game.clients.get(msg.nick)
-		if(replacedClient === undefined) {
-			console.log("Client not connected", msg.nick)
-		}
-		replacedClient!.isOnline((alive) => {
-			if (alive) {
-				console.log("Can't reconnect when client is online", msg.nick)
-			} else {
-				console.log("Player reconnected", msg.nick)
-				game.clients.set(msg.nick, client)
-				if(game.getActivePlayer() === msg.nick) {
-					notifyNextPlayer()
-				} 
-				ws.send(JSON.stringify({ type: "NICK", nick: msg.nick }))
-			}
-		}) 
-	} else {
-		let nick = nicks[currentTurn]
-		currentTurn++
-		game.clients.set(nick, client)
-		game.order.push(nick)
-	
-		console.log("Registered", nick, game.clients.size)
-		ws.send(JSON.stringify({ type: "NICK", nick: nick }))
-		const newPlayerMessage: NewPlayerMessage = { type: "newPlayer", nick }
-		game.clients.get("host")?.sendHost(newPlayerMessage)
-	}
-}
+  if (game.isInProgress()) {
+    const replacedClient = game.clients.get(msg.nick);
+    if (replacedClient === undefined) {
+      console.log("Client not connected", msg.nick);
+    }
+    replacedClient!.isOnline((alive) => {
+      if (alive) {
+        console.log("Can't reconnect when client is online", msg.nick);
+      } else {
+        console.log("Player reconnected", msg.nick);
+        game.clients.set(msg.nick, client);
+        if (game.getActivePlayer() === msg.nick) {
+          notifyNextPlayer();
+        }
+        ws.send(JSON.stringify({ type: "NICK", nick: msg.nick }));
+      }
+    });
+  } else {
+    let nick = nicks[currentTurn];
+    currentTurn++;
+    game.clients.set(nick, client);
+    game.order.push(nick);
+
+    console.log("Registered", nick, game.clients.size);
+    ws.send(JSON.stringify({ type: "NICK", nick: nick }));
+    const newPlayerMessage: NewPlayerMessage = { type: "newPlayer", nick };
+    game.clients.get("host")?.sendHost(newPlayerMessage);
+  }
+};
 
 const handlePawnRegister = (msg: PawnRegisterMessage, ws: WebSocket) => {
-	if (game.pawns.has(msg.nick)) {
-		// TODO: handle on reconnect
-		console.error("Pawn already registered")
-		return
-	}
-	game.pawns.set(msg.nick, ws)
+  if (game.pawns.has(msg.nick)) {
+    // TODO: handle on reconnect
+    console.error("Pawn already registered");
+    return;
+  }
+  game.pawns.set(msg.nick, ws);
 
-	// when all connected clients except 'host' have registered their pawns
-	if(game.pawns.size + 1 === game.clients.size) {
-		game.start() 
-		notifyNextPlayer()
-	}
+  // when all connected clients except 'host' have registered their pawns
+  if (game.pawns.size + 1 === game.clients.size) {
+    game.start();
+    notifyNextPlayer();
+  }
 
-
-	console.log(`Pawn registered: ${msg.nick}, ${game.pawns.size} of ${game.clients.size} pawns registered`)
-}
+  console.log(
+    `Pawn registered: ${msg.nick}, ${game.pawns.size} of ${game.clients.size} pawns registered`
+  );
+};
 
 const handleMovePawn = (nick: string, fieldsToMove: number) => {
-	const ws = game.pawns.get(nick)
-	console.log("Move pawn", nick, fieldsToMove)
-	ws?.send(JSON.stringify({ type: "movePawn", fieldsToMove: fieldsToMove, nick: nick }))
-}
+  const ws = game.pawns.get(nick);
+  console.log("Move pawn", nick, fieldsToMove);
+  ws?.send(
+    JSON.stringify({ type: "movePawn", fieldsToMove: fieldsToMove, nick: nick })
+  );
+};
 
 const handleQuestion = (msg: QuestionMessage) => {
-	const ws = game.validateQuestion(msg.nick)
+  const ws = game.validateQuestion(msg.nick);
 
-	if(ws) 
-		ws.send(JSON.stringify({ type: "question" }))
-}
+  if (ws) ws.send(JSON.stringify({ type: "question" }));
+};
 
 const handleMessageToClient = (msg: ServerMessage, ws: WebSocket) => {
-	ws.send(JSON.stringify(msg))
-}
+  ws.send(JSON.stringify(msg));
+};
 
 export const handleDiceThrow = (msg: DiceThrowMessage) => {
-	if (game.validateDiceThrow(msg.nick, msg.dice)) {
-		handleMovePawn(msg.nick, msg.dice)
+  if (game.validateDiceThrow(msg.nick, msg.dice)) {
+    handleMovePawn(msg.nick, msg.dice);
 
-		handleQuestion({ type: "question", nick: game.order[0] }) // TEST FLOW! REMOVE LATER!
-	}
-}
+    handleQuestion({ type: "question", nick: game.order[0] }); // TEST FLOW! REMOVE LATER!
+  }
+};
 
 export const handleAnswer = (msg: AnswerMessage) => {
-	const who = game.getActivePlayer()
-	if(game.validateAnswer(who, msg.answer))	{
-		notifyNextPlayer()
-		if (msg.answer === "Timeout") {
-			game.clients.get(who)?.send({ type: "timeout" })
-		}
-	}
-}
+  const who = game.getActivePlayer();
+  if (game.validateAnswer(who, msg.answer)) {
+    notifyNextPlayer();
+    if (msg.answer === "Timeout") {
+      game.clients.get(who)?.send({ type: "timeout" });
+    }
+  }
+};
 
 const notifyNextPlayer = () => {
-	const next = game.clients.get(game.order[0])
+  const next = game.clients.get(game.order[0]);
 
-	if(next == undefined) {
-		console.error("No next player")
-		return
-	}
-	next.send({ type: "throwDice" })
-
-}
+  if (next == undefined) {
+    console.error("No next player");
+    return;
+  }
+  next.send({ type: "throwDice" });
+};
