577cfdf8481ceaf56c44b155b146d5f8e7bd874e
Kacper Fus <fuskacper@student.agh.edu.pl>
2024-04-17 13:49:15 +0200

    home page and instruction corrected

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -5,27 +5,32 @@ import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <React.Fragment>
-      <Head>
-        <title>Home - Nextron (with-tailwindcss)</title>
-      </Head>
-      <div className="grid grid-col-1 p-5 text-2xl w-full text-center">
-        <div className="p-5">
-          <Image
-            className="ml-auto mr-auto"
-            src="/images/logo.png"
-            alt="Logo image"
-            width="256px"
-            height="256px"
-          />
-        </div>
-        <div className="p-5 text-3xl">Welcome to Nextron!</div>
-      </div>
-      <div className="mt-1 w-full flex-wrap flex justify-center">
-        <Link href="/loader">
-          <a className="btn-blue">Rozpocznij przygodę!</a>
-        </Link>
-      </div>
-    </React.Fragment>
+      <React.Fragment>
+          <Head>
+              <title>Home - Nextron (with-tailwindcss)</title>
+          </Head>
+          <div className="grid grid-col-1 p-5 text-2xl w-full text-center">
+              <div className="p-5">
+                  <Image
+                      className="ml-auto mr-auto"
+                      src="/images/logo.png"
+                      alt="Logo image"
+                      width="256px"
+                      height="256px"
+                  />
+              </div>
+              <div className="p-5 text-4xl" style={{fontSize: '3rem'}}>
+                  Witaj w Nextron!
+                  <div className="my-5 text-2xl" style={{fontSize: '2rem'}}>
+                      Zmień lekcje w przygodę - nauczaj poprzez zabawę!
+                  </div>
+              </div>
+          </div>
+          <div className="mt-1 w-full flex-wrap flex justify-center">
+              <Link href="/loader">
+                  <a className="btn-blue">Rozpocznij przygodę!</a>
+              </Link>
+          </div>
+      </React.Fragment>
   );
 }
--- renderer/pages/hotspot_instruction_page/index.tsx
+++ renderer/pages/hotspot_instruction_page/index.tsx
@@ -13,16 +13,6 @@ export default function hotspotPage() {
         <div className="flex min-w-full flex-col text-4xl" >
             <div className='flex'>
                 <div className="m-10">
-                    Dla użytkowników MacOs:
-                    <li>Klikamy w ikonę apple</li>
-                    <li>Wybieramy 'Ustawienia systemowe'</li>
-                    <li>Kliknij w 'ogólne' na pasku bocznym</li>
-                    <li>kliknij w 'Udostępnianie' po prawej</li>
-                    <li>Włącz Udostępnianie Internetu, a następnie kliknij we Włącz, aby potwierdzić.</li>
-
-                </div>
-                <div className="m-10">
-                    Dla użytkowników windowsa:
                     <li>Klikamy przycisk windows</li>
                     <li>Wyszukujemy 'hotspot'</li>
                     <li>Następnie klikamy w zakładce hotspot mobilny: włącz</li>
